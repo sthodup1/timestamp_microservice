c9a22fb843a6340d1b29ddf779f7c8648c59eaa7
sthodupunuri <sthodupunuri@gmail.com>
2016-07-15 19:08:16 +0000

    Working copy

--- server.js
+++ server.js
@@ -1,11 +1,34 @@
 var express = require("express");
-
+var moment = require("moment");
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('env', 'production');
+app.set('views', __dirname);
+app.set('view engine', 'jade');
+
+app.get('/', function(req, res) {
+  res.render('index');
+})
+
+app.get('/:date', function (req, res) {
+  //.send(req.params.date);
+  var myDate;
+  
+  if(!isNaN(req.params.date)) {
+    myDate = moment(req.params.date, "X");
+  } else {
+    myDate = moment(req.params.date, "MMMM D, YYYY");
+  }
+  
+  var toSend = {unix : null, natural : null};
+  
+  if(myDate.isValid()) {
+    toSend.unix = myDate.format('X');
+    toSend.natural = myDate.format('MMMM, D YYYY')
+  }
+  res.json(toSend);
 });
 
 app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+  console.log('App is listening on port 8080!');
 });
